ac9031bb96d0813a7465234e189f5d7fcc9fa81a
Michael Sumner <mdsumner@gmail.com>
2024-03-03 06:08:58 +0000

    what a relief

--- scripts/install_gdal-dev.sh
+++ scripts/install_gdal-dev.sh
@@ -234,7 +234,7 @@ install2.r --error --skipmissing -n "$NCPUS" -r "${CRAN_SOURCE}" \
     crew.cluster
 
 
-Rscript -e 'devtools::install_github(c("hypertidy/vapour", "hypertidy/PROJ", "hypertidy/ximage", "hypertidy/sds", "hypertidy/dsn"), Ncpus = 4)'
+Rscript -e 'devtools::install_github(c("hypertidy/whatarelief", "hypertidy/vapour", "hypertidy/PROJ", "hypertidy/ximage", "hypertidy/sds", "hypertidy/dsn"), Ncpus = 4)'
 
 Rscript -e 'devtools::install_github(c("AustralianAntarcticDivision/palr", "AustralianAntarcticDivision/raadfiles", "AustralianAntarcticDivision/raadtools", "AustralianAntarcticDivision/blueant", "ropensci/bowerbird"), Ncpus = 4)'
 
