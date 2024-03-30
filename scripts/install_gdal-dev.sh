4e6211a0f47a77d34f4f074f8406557cbaabecfd
Michael Sumner <mdsumner@gmail.com>
2024-03-30 08:40:14 +0000

    add fasterize

--- scripts/install_gdal-dev.sh
+++ scripts/install_gdal-dev.sh
@@ -246,6 +246,8 @@ Rscript - 'devtools::install_github("geoarrow/geoarrow-r")'
 
 Rscript - 'devtools::install_github("paleolimbot/geoarrow")'
 
+Rscript - 'devtools::install_cran("fasterize")'
+
 ## use the SCAR r-universe package repository
 Rscript -e 'op <- options(repos = c(SCAR = "https://scar.r-universe.dev", CRAN = "https://cloud.r-project.org")); install.packages("bowerbird", Ncpus = 4); options(op)'
 
