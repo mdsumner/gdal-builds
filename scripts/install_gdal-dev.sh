0a9435725e1330e09ae2cb0bcfc07b5fd11e98d0
Michael Sumner <mdsumner@gmail.com>
2024-12-24 15:32:38 +1100

    Update install_gdal-dev.sh
    sooty

--- scripts/install_gdal-dev.sh
+++ scripts/install_gdal-dev.sh
@@ -300,7 +300,9 @@ Rscript -e 'devtools::install_github(c("AustralianAntarcticDivision/palr", "Aust
 
 Rscript -e 'devtools::install_github("eliocamp/rcmip6")'
 
-#Rscript -e 'devtools::install_github(c("hypertidy/anglr", "mdsumner/sooty"))'
+#Rscript -e 'devtools::install_github(c("hypertidy/anglr"))'
+
+Rscript -e 'devtools::install_github(c("mdsumner/sooty"))'
 
 Rscript -e 'devtools::install_github("geoarrow/geoarrow-r")'
 
